d4bdb70df0e7282d0be800debfbd9c57b99c7208
Elina <e@dimante.com>
2020-02-12 12:15:46 +0000

    Adding examples of conditionals

--- conditionals.py
+++ conditionals.py
@@ -30,3 +30,75 @@ print(calculate_storage(1))    # Should be 4096
 print(calculate_storage(4096)) # Should be 4096
 print(calculate_storage(4097)) # Should be 8192
 print(calculate_storage(6000)) # Should be 8192
+
+#test4
+def color_translator(color):
+	if color == "red":
+		hex_color = "#ff0000"
+	elif color == "green":
+		hex_color = "#00ff00"
+	elif color == "blue":
+		hex_color = "#0000ff"
+	else:
+		hex_color = "unknown"
+	return hex_color
+
+print(color_translator("blue")) # Should be #0000ff
+print(color_translator("yellow")) # Should be unknown
+print(color_translator("red")) # Should be #ff0000
+print(color_translator("black")) # Should be unknown
+print(color_translator("green")) # Should be #00ff00
+print(color_translator("")) # Should be unknown
+
+#test5
+def format_name(first_name, last_name):
+	# code goes here
+	if first_name is not "" and last_name is not "":
+	   return "Name: " + last_name +" "+ first_name
+	elif first_name is not "" and last_name == "":
+	   return "Name: " + first_name
+	elif last_name is not "" and first_name == "":
+	   return "Name: " + last_name
+	else:
+	   return ""
+
+print(format_name("Ernest", "Hemingway"))
+# Should be "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should be "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should be "Name: Voltaire"
+
+print(format_name("", ""))
+# Should be ""
+
+#test6
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word3) and len(word2) >= len(word1):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
+
+#test7
+def fractional_part(numerator, denominator):
+	# Operate with numerator and denominator to 
+  # keep just the fractional part of the quotient
+  if denominator == 0:
+    return 0
+  return (numerator / denominator) - (numerator // denominator)
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
