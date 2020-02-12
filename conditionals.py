347de8c3153b7f248d4ce14f4991b07ef4d00e3a
Elina <e@dimante.com>
2020-02-12 12:15:12 +0000

    Adding examples of conditionals

---
+++ conditionals.py
@@ -0,0 +1,32 @@
+# Conditionals
+
+# test 1
+def greeting(name):
+  if name == "Taylor":
+    return "Welcome back Taylor!"
+  else:
+    return "Hello there, " + name
+
+print(greeting("Taylor"))
+print(greeting("John"))
+
+# test 2
+print("A dog " < "A mouse")
+print(9999+8888 > 100*100)
+
+#test 3
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = filesize // block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = filesize % block_size
+    # Depending on whether there's a remainder or not, return the right number.
+    if partial_block_remainder > 0:
+        return (block_size*full_blocks)+block_size
+    return block_size * full_blocks
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
