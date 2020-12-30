2a2cbf3e3a45d9a4f428660a721e52f075241232
Elina Dimante <elina.dimante@gmail.com>
2020-12-30 22:20:38 +0000

    Adding practice quiz exercisws to my Regular expressions notes file

--- M2W3_Regular_Expressions.py
+++ M2W3_Regular_Expressions.py
@@ -199,7 +199,7 @@ print(check_sentence("A star is born.")) # True
 
 import re
 def check_web_address(text):
-  pattern = ___
+  pattern = r"[\.][A-Za-z]*$"
   result = re.search(pattern, text)
   return result != None
 
@@ -208,3 +208,50 @@ print(check_web_address("www@google")) # False
 print(check_web_address("www.Coursera.org")) # True
 print(check_web_address("web-address.com/homepage")) # False
 print(check_web_address("My_Favorite-Blog.US")) # True
+
+# The check_time function checks for the time format of a 12-hour clock, as follows: 
+# the hour is between 1 and 12, with no leading zero, followed by a colon, then minutes 
+# between 00 and 59, then an optional space, and then AM or PM, in upper or lower case. 
+# Fill in the regular expression to do that. How many of the concepts that you just learned can you use here?
+
+import re
+def check_time(text):
+  pattern = r"[1-9]+:\d[1-9]\s*[Aa]|[Pp][Mm]"
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
+
+# The contains_acronym function checks the text for the presence of 2 or more characters or digits surrounded by parentheses, 
+# with at least the first character in uppercase (if it's a letter), returning True if the condition is met, or False otherwise. 
+# For example, "Instant messaging (IM) is a set of communication technologies used for text-based communication" 
+# should return True since (IM) satisfies the match conditions." Fill in the regular expression in this function: 
+
+import re
+def contains_acronym(text):
+  pattern = ___ 
+  result = re.search(pattern, text)
+  return result != None
+
+print(contains_acronym("Instant messaging (IM) is a set of communication technologies used for text-based communication")) # True
+print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication")) # True
+print(contains_acronym("Please do NOT enter without permission!")) # False
+print(contains_acronym("PostScript is a fourth-generation programming language (4GL)")) # True
+print(contains_acronym("Have fun using a self-contained underwater breathing apparatus (Scuba)!")) # True
+
+# Fill in the code to check if the text passed includes a possible U.S. zip code, formatted as follows: 
+# exactly 5 digits, and sometimes, but not always, followed by a dash with 4 more digits. 
+# The zip code needs to be preceded by at least one space, and cannot be at the start of the text.
+
+import re
+def check_zip_code (text):
+  result = re.search(r".* [0-9]{5}", text)
+  return result != None
+
+print(check_zip_code("The zip codes for New York are 10001 thru 11104.")) # True
+print(check_zip_code("90210 is a TV show")) # False
+print(check_zip_code("Their address is: 123 Main Street, Anytown, AZ 85258-0001.")) # True
+print(check_zip_code("The Parliament of Canada is at 111 Wellington St, Ottawa, ON K1A0A9.")) # False
