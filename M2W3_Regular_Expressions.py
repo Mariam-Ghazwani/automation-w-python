24af85890dbd25525d6ee7dcdd2f76ebaaf83de0
Elina Dimante <elina.dimante@gmail.com>
2021-01-01 18:44:20 +0000

    Uploading very last version of RE notes and qwiklabs task

--- M2W3_Regular_Expressions.py
+++ M2W3_Regular_Expressions.py
@@ -255,3 +255,205 @@ print(check_zip_code("The zip codes for New York are 10001 thru 11104.")) # True
 print(check_zip_code("90210 is a TV show")) # False
 print(check_zip_code("Their address is: 123 Main Street, Anytown, AZ 85258-0001.")) # True
 print(check_zip_code("The Parliament of Canada is at 111 Wellington St, Ottawa, ON K1A0A9.")) # False
+
+
+# Advanced Regular Expressions
+# Capturing Groups
+
+result = re.search(r"^(\w*), (\w*)$", "Lovelace, Ada")
+print(result)
+# match = 'Lovelace, Ada)
+print(result.groups()
+('Lovelace', 'Ada') # Because we defined two separate groups, the group method returns a tuple of two elements.
+print(result[0])
+Lovelace, Ada
+"{} {}".format(result[2], result[1])
+'Ada Lovelace'
+
+def rearrange_name(name):
+    result = re.search(r"^([\w \.-]*), ([\w \.-]*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
+# More on Repetition Qualifiers
+print(re.search(r"[a-zA-Z]{5}", "a ghost"))
+match = 'ghost'
+print(re.findall(r"[a-zA-Z]{5}", "a scary ghost appeared"))
+match = ['scary', 'ghost', 'appea']
+# if we wanted to match all the words that are exactly five letters long? We can do that using \b, which matches word limits
+#  at the beginning and end of the pattern, to indicate that we want full words
+print(re.findall(r"\b[a-zA-Z]{5}\b", "a scary ghost appeared"))
+match = ['scary', 'ghost']
+# if we wanted to match a range of five to ten letters or numbers, we could use an expression like this one:
+print(re.findall(r"\w{5,10}", "I really like strawberries"))
+match = ['really', 'strawberri']
+print(re.findall(r"\w{5,}", "I really like strawberries"))
+match = ['really', 'strawberries']
+# Here we look for a pattern that was an S followed by up to 20 alphanumeric characters.
+print(re.search(r"s\w{","20}", "I really like strawberries"))
+match = ['strawberries']
+
+# More on Repetition Qualifiers task
+# The long_words function returns all words that are at least 7 characters. Fill in the regular expression to complete this function.
+
+import re
+def long_words(text):
+  pattern = r"\w{7,}"
+  result = re.findall(pattern, text)
+  return result
+
+print(long_words("I like to drink coffee in the morning.")) # ['morning']
+print(long_words("I also have a taste for hot chocolate in the afternoon.")) # ['chocolate', 'afternoon']
+print(long_words("I never drink tea late at night.")) # []
+
+# Extracting a PID Using regexes in Python
+
+import re
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]" '''After the square bracket, comes the first parentheses. Since it isn't escaped, we know it'll be used as a capturing group. 
+The capturing group parentheses are wrapping the backslash d+ symbols. From our discussion of special characters and repetition qualifiers, 
+we know that this expression will match one or more numerical characters.'''
+result = re.search(regex, log)
+print(result[1]) # After calling the search function, we know that because we're capturing groups in an expression, we can access the matching data by accessing the value at index 1. 
+
+# We should have a function that extracts the process ID or PID when possible, and does something else if not. It's something like this; will start by defining a function called extract_pid.
+
+import re
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]"
+    result = re.search(regex, log_line)
+    if result is None:
+        return ""
+    return result[1]
+
+print(extract_pid(log))
+
+# Question/task
+# Add to the regular expression used in the extract_pid function, to return the uppercase message in parenthesis, after the process id.
+import re
+def extract_pid(log_line):
+    regex = r"(\[(\d+)\]).*\s([A-Z]*)\s"
+    result = re.search(regex, log_line)
+    if result is None:
+        return None
+    return "{} ({})".format(result[2], result[3])
+
+print(extract_pid("July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade")) # 12345 (ERROR)
+print(extract_pid("99 elephants in a [cage]")) # None
+print(extract_pid("A string that also has numbers [34567] but no uppercase message")) # None
+print(extract_pid("July 31 08:08:08 mycomputer new_process[67890]: RUNNING Performing backup")) # 67890 (RUNNING)
+
+# Splitting and Replacing
+re.split(r"[.?!]", "One sentence. Another one? And the last one!")
+['One sentence', 'Another one', 'And the last one', '']
+re.split(r"([.?!])", "One sentence. Another one? And the last one!")
+['One sentence', '.', 'Another one', '?', 'And the last one', '!', '']
+
+# Another interesting function provided by the RE module is called sub.
+# It's used for creating new strings by substituting all or part of them for a different string, 
+# similar to the replace string method but using regular expressions for both the matching and the replacing.
+
+re.sub(r"[\w.%+-]+@[\w.-]+", "[REDACTED]", "Received an email for go_nuts95@my.example.com")
+Received an email for [REDACTED]
+
+re.sub(r"^([\w .-]*), ([\w .-]*)$", r"\2 \1", "Lovelace, Ada")
+'Ada Lovelace''
+
+
+# If we want to make sure that only the actual words are used to split the text, we need to use the escape character 
+# or word boundaries, like this: r"\bthe\b|\ba\b". Otherwise, any instance of the given words is used as delimiters, 
+# no matter where they are in the text, even in the middle of other words like "Another" and "last".
+
+re.split(r"the|a", "One sentence. Another one? And the last one!")
+['One sentence. Ano', 'r one? And ', ' l', 'st one!']
+
+# Practice Quiz: Advanced Regular Expressions
+# 1
+# We're working with a CSV file, which contains employee information. Each record has a name field, followed by a phone 
+# number field, and a role field. The phone number field contains U.S. phone numbers, and needs to be modified to the 
+# international format, with "+1-" in front of the phone number. Fill in the regular expression, using groups, to use 
+# the transform_record function to do that.
+
+import re
+def transform_record(record):
+  new_record = re.sub(r"(\d{3}-\d{3}-?\d{4})", r"+1-\1", record)
+  return new_record
+
+print(transform_record("Sabrina Green,802-867-5309,System Administrator")) 
+# Sabrina Green,+1-802-867-5309,System Administrator
+
+print(transform_record("Eli Jones,684-3481127,IT specialist")) 
+# Eli Jones,+1-684-3481127,IT specialist
+
+print(transform_record("Melody Daniels,846-687-7436,Programmer")) 
+# Melody Daniels,+1-846-687-7436,Programmer
+
+print(transform_record("Charlie Rivera,698-746-3357,Web Developer")) 
+# Charlie Rivera,+1-698-746-3357,Web Developer
+
+
+# 2
+# The multi_vowel_words function returns all words with 3 or more consecutive vowels (a, e, i, o, u). Fill in the regular expression to do that.
+
+import re
+def multi_vowel_words(text):
+  pattern = r"(\w*[aeiou]{3}\w*)"
+  result = re.findall(pattern, text)
+  return result
+
+print(multi_vowel_words("Life is beautiful")) 
+# ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
+# ['queue']
+
+print(multi_vowel_words("Hello world!")) 
+# []
+
+# 3
+# The transform_comments function converts comments in a Python script into those usable by a C compiler. 
+# This means looking for text that begins with a hash mark (#) and replacing it with double slashes (//), 
+# which is the C single-line comment indicator. For the purpose of this exercise, we'll ignore the possibility 
+# of a hash mark embedded inside of a Python command, and assume that it's only used to indicate a comment. 
+# We also want to treat repetitive hash marks (##), (###), etc., as a single comment indicator, to be 
+# eplaced with just (//) and not (#//) or (//#). Fill in the parameters of the substitution method to complete this function: 
+
+import re
+def transform_comments(line_of_code):
+  result = re.sub(r"#+", r"//", line_of_code)
+  return result
+
+print(transform_comments("### Start of program")) 
+# Should be "// Start of program"
+print(transform_comments("  number = 0   ## Initialize the variable")) 
+# Should be "  number = 0   // Initialize the variable"
+print(transform_comments("  number += 1   # Increment the variable")) 
+# Should be "  number += 1   // Increment the variable"
+print(transform_comments("  return(number)")) 
+# Should be "  return(number)"
+
+# 4
+# The convert_phone_number function checks for a U.S. phone number format: XXX-XXX-XXXX 
+# (3 digits followed by a dash, 3 more digits followed by a dash, and 4 digits), and 
+# converts it to a more formal format that looks like this: (XXX) XXX-XXXX. 
+# Fill in the regular expression to complete this function.
+
+import re
+def convert_phone_number(phone):
+  result = re.sub(r"(\d{3})-(\d{3}-\d{4}\b)", r"(\1) \2", phone)
+  return result
+
+print(convert_phone_number("My number is 212-345-9999.")) # My number is (212) 345-9999.
+print(convert_phone_number("Please call 888-555-1234")) # Please call (888) 555-1234
+print(convert_phone_number("123-123-12345")) # 123-123-12345
+print(convert_phone_number("Phone number of Buckingham Palace is +44 303 123 7300")) # Phone number of Buckingham Palace is +44 303 123 7300
+
