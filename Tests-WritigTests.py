b1ac735611a600ca3f1bb25e5a26fb880f6fe0b5
Elina Dimante <elina.dimante@gmail.com>
2021-03-17 16:06:25 +0000

    Adding new chapter on Testing in Python

---
+++ Tests-WritigTests.py
@@ -0,0 +1,10 @@
+
+# Unit tests
+'''Unit tests are used to verify that small isolated parts of a program are correct.
+Unit tests are generally written alongside the code to test the behavior of 
+individual pieces or units like functions or methods. '''
+
+# Edge Case
+'''Edge cases are inputs to our code that produce unexpected results, and are found 
+at the extreme ends of the ranges of input we imagine our programs will typically work with. 
+Edge cases usually need special handling in scripts in order for the code to continue to behave correctly. '''
